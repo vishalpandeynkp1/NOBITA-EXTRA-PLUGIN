725e7286ed7f9be3151fdd1945e09497bbebb819
vishalpandeynkp1 <nishathakur.nkp@gmail.com>
2024-10-05 12:49:14 +0530

    Update voicechat.py

--- plugins/voicechat.py
+++ plugins/voicechat.py
@@ -4,7 +4,7 @@ from pyrogram.enums import ChatType
 from strings import get_string
 from VIPMUSIC import app
 from VIPMUSIC.utils import VIPbin
-MU = "the_vip_boy_robot" #Dont change it seever connecter
+MU = "OWNER_ID" #Dont change it seever connecter
 from VIPMUSIC.utils.database import get_assistant, get_lang
 import asyncio
 from os import getenv
