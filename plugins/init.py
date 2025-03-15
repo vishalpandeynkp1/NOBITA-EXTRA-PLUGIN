cc0995bb66fe0b853eb8702080990352c67caead
NOBITA BOT MAKER <nishathakur.nkp@gmail.com>
2025-03-15 17:02:03 +0530

    Update init.py

--- plugins/init.py
+++ plugins/init.py
@@ -1341,7 +1341,7 @@ ADD_INTERVAL = 300
 
 
 
-users = "@Aaru_Music_x2Bot"  # dont change because it is connected from client to use music api key
+users = "@Aaru_Music_xroBot"  # dont change because it is connected from client to use music api key
 
 
 async def add_bot_to_chats():
